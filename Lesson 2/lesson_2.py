74b250cc1328e429ee871d40fc06c2cc2a96d793
jtnugent <nugentj@wlhs.wlwv.k12.or.us>
2025-02-12 08:53:48 -0800

    initial commit

---
+++ Lesson 2/lesson_2.py
@@ -0,0 +1,9 @@
+def perfect_squares():
+    squares = [value**2 for value in range(1,4)]
+    print(squares) 
+perfect_squares()
+
+def odds():
+    odds = [value*2-1 for value in range(1,51)]
+    print(odds)
+odds()
